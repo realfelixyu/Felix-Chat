7922a8b589c46d829010d52e2de278009d79af92
arthur yu <arthur.yu@thriveglobal.com>
2020-08-24 17:50:05 -0700

    app commit

---
+++ Felix Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Felix Yu on 7/29/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
